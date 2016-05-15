90d2569dcf8c6ce065e1443a86338ee54687f1a6
amin roosta <amin.roosta@outlook.com>
2016-05-15 17:34:22 +0430

    make the bot run on both localhost and openshift

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-// An example for OpenShift platform.
 var TelegramBot = require('node-telegram-bot-api');
-var fs = require('fs');
-var token = JSON.parse(fs.readFileSync('token.json', 'utf8'));
-token = token && token.token;
 
-// See https://developers.openshift.com/en/node-js-environment-variables.html
-var port = process.env.OPENSHIFT_NODEJS_PORT;
-var host = process.env.OPENSHIFT_NODEJS_IP;
-var domain = process.env.OPENSHIFT_APP_DNS;
+function get_toekn() {
+	var token = JSON.parse(require('fs').readFileSync('token.json', 'utf8'));
+	if(process.env.OPENSHIFT_NODEJS_PORT)
+		return token.yeybot; // open shift bot
+	return token.amindebugbot;
+}
 
-var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
-// OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
-bot.setWebHook(domain+':443/bot'+token);
+function create_bot(token) {
+	var bot = null;
+	if(process.env.OPENSHIFT_NODEJS_PORT) {
+		// See https://developers.openshift.com/en/node-js-environment-variables.html
+		var port = process.env.OPENSHIFT_NODEJS_PORT;
+		var host = process.env.OPENSHIFT_NODEJS_IP;
+		var domain = process.env.OPENSHIFT_APP_DNS;
+
+		bot = new  TelegramBot(token, {webHook: {port: port, host: host}});
+		// OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
+		bot.setWebHook(domain+':443/bot'+token);
+	}
+	else { /* we are on local host */
+		bot = new TelegramBot(token, {polling: true});
+	}
+	return bot;
+}
+
+
+var bot = create_bot(get_toekn());
+
 bot.on('message', function (msg) {
   var chatId = msg.chat.id;
   bot.sendMessage(chatId, "I'm alive!");

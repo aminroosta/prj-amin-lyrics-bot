c996c444755daf1feeec0e35090647c6049a9e4a
amin roosta <amin.roosta@outlook.com>
2016-05-15 21:29:51 +0430

    implemented basic api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var TelegramBot = require('node-telegram-bot-api');
 var api = require('./lyrics_api.js');
+var LocalStorage = require('node-localstorage').LocalStorage;
+var localStorage = new LocalStorage('./database');
 
 function get_toekn() {
 	var token = JSON.parse(require('fs').readFileSync('token.json', 'utf8'));
@@ -31,17 +33,69 @@ function run_bot() {
 	var bot = create_bot(get_toekn());
 
 	bot.on('message', function (msg) {
-	  var chatId = msg.chat.id;
-	  bot.sendMessage(chatId, "I'm alive!");
+		var chatId = msg.chat.id;
+		var text = msg.text;
+		if(!text) {
+			bot.sendMessage(chatId, "Type the name of a song to search for.");
+			return;
+		}
+
+
+		var number = text * 1;
+		if(number) {
+			var arr = localStorage.getItem(chatId);
+			arr = arr && JSON.parse(arr);
+			if(!arr || !arr[number]) {
+				bot.sendMessage(chatId, 'Oops!\n No previous song with this number is found.');
+				return;
+			}
+
+			bot.sendMessage(chatId, 'OK, i will be back with your lyric in a second :)');
+			var url = arr[number];
+			api.lyric(url)
+			   .then(function(text){
+				  bot.sendMessage(chatId, text);
+			   })
+				.catch(function(err) {
+					bot.sendMessage(chatId, 'Oops! i\'m sorry!\n Something went wrong.');
+				});
+			return;
+		}
+
+
+
+		bot.sendMessage(chatId, 'OK, i will be back right away, query = ' + text );
+
+		api.search(text)
+			.then(function(arr){
+				if(arr.length == 0){
+					bot.sendMessage(chatId, 'Oops!\n I couldn\'t find anything for "' + text + '"');
+					return;
+				}
+
+				var list = 'Ok, i\'ve found these songs, type song number to get your lyric\n\n';
+				var number = 1;
+				arr.forEach(function(href){
+			    	href = href.replace('http://www.azlyrics.com/lyrics/','').replace('.html','').split('/');
+			    	var artist = href[0];
+			    	var song = href[1];
+		    		list += number + ' : ' + song + ' - ' + artist + '\n';
+		    		number += 1;
+				});
+
+				bot.sendMessage(chatId, list);
+				localStorage.setItem(chatId, JSON.stringify(arr));
+			})
+			.catch(function(err) {
+				bot.sendMessage(chatId, 'Oops! i\'m sorry!\n Something went wrong.');
+			});
 	});
 }
 
-//run_bot();
+run_bot();
 
 
-api.search('going under')
-	.then(t => console.log(t))
-	.catch(function(err) {console.warn(err); });
+// api.search('going under') .then(t => console.log(t)) .catch(function(err) {console.warn(err); });
 // lyric('http://www.azlyrics.com/lyrics/survivor/eyeofthetiger.html').catch(function(err) {console.warn(err); });
 
 

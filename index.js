28c2cf4a070e00c2fb3cf104c43f308762007fbe
amin roosta <amin.roosta@outlook.com>
2016-05-15 17:11:30 +0430

    node-telegram-bot example

---
+++ index.js
@@ -0,0 +1,19 @@
+// An example for OpenShift platform.
+var TelegramBot = require('node-telegram-bot-api');
+var fs = require('fs');
+var token = JSON.parse(fs.readFileSync('token.json', 'utf8'));
+token = token && token.token;
+
+// See https://developers.openshift.com/en/node-js-environment-variables.html
+var port = process.env.OPENSHIFT_NODEJS_PORT;
+var host = process.env.OPENSHIFT_NODEJS_IP;
+var domain = process.env.OPENSHIFT_APP_DNS;
+
+var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+// OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
+bot.setWebHook(domain+':443/bot'+token);
+bot.on('message', function (msg) {
+  var chatId = msg.chat.id;
+  bot.sendMessage(chatId, "I'm alive!");
+});
+
